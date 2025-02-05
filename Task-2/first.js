41f63497993430b20d644b8884564e132d0fc32b
MizanRbf <mizanrbf@gmail.com>
2025-02-05 14:30:25 +0600

    task-2 completed

---
+++ Task-2/first.js
@@ -0,0 +1,9 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var english = 99.50;
+var totalMarks = mathematics + biology + chemistry + physics + english;
+var averageMarks = totalMarks / 5;
+
+console.log(averageMarks);
